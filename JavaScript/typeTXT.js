fbc87f543b10d2c34f4cf0b9b6bcf738503256a3
Nicholas O'Brien <nicholas.obrien@ucc.on.ca>
2016-06-12 20:47:37 -0400

    modularized html files

--- JavaScript/typeTXT.js
+++ JavaScript/typeTXT.js
@@ -1,6 +1,15 @@
+$.ajax({
+    url: 'typeTXT.html',
+    type: 'get',
+    async: false,
+    success: function(html) {
+        var text = html;
+    }
+});
+
 $(function(){
     $(".txt").typed({
-        strings: ["^1500 Hi, I'm Nick. I'm a grade 9 student in Toronto, Canada.<br><br>I like to row, sail, and program.<br><br>I've put most of my projects on <a href='github.com/OBRIEN66'>Github</a>.<br><br> "],
+        strings: text,
         typeSpeed: 0,
         contentType: "html",
         cursorChar: "",

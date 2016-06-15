222b75d6955a92906249af9782bc9bdbb41cfd1d
Nicholas O'Brien <nicholas.obrien@ucc.on.ca>
2016-06-14 21:53:35 -0400

    Links work now

--- JavaScript/text.js
+++ JavaScript/text.js
@@ -9,12 +9,12 @@ $(function(){
     $(".text").typed({
         strings: ["^2000 Hi, I'm Nick. I'm a grade 10 student living in Toronto, Canada.<br><br>I like to row,"
         + " sail, and program.<br><br>I've put most of my projects on "
-        + "<a href='https://github.com/OBRIEN66'>Github</a>.<br><br>Namely,"
-        + " <ul><li><a href='https://github.com/OBRIEN66/terminal-like-site' target='_blank'>This</a></li>"
-        + "<li><a href='https://obrien.tech' target='_blank'>My other site</a></li></ul><br>I'm a part of the "
-        + "<a href='https://robotics.ucc.on.ca' target='_blank'>robotics club</a> at my school and"
+        + "<a href='http://github.com/OBRIEN66'>Github</a>.<br><br>Namely,"
+        + " <ul><li><a href='http://github.com/OBRIEN66/terminal-like-site' target='_blank'>This</a></li>"
+        + "<li><a href='http://obrien.tech' target='_blank'>My other site</a></li></ul><br>I'm a part of the "
+        + "<a href='http://robotics.ucc.on.ca' target='_blank'>robotics club</a> at my school and"
         + " I did a little bit of work on the site. <br><br> "
-        + "This year, I will take part in <a href='https://world.ac' target='_blank'>World Affairs Conference</a>"
+        + "This year, I will take part in <a href='http://world.ac' target='_blank'>World Affairs Conference</a>"
         + " and Horizons Robotics<br><br>"
         + "If you want to contact me, I am accessible by "
         + "<a href='mailto:nicholas.obrien@ucc.on.ca'>email</a> (nicholas.obrien@ucc.on.ca)"],

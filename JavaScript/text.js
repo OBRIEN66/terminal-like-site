e50ae8554c5c71145057792b190623daa83f31ef
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2016-10-11 11:21:11 -0400

    Updated text

--- JavaScript/text.js
+++ JavaScript/text.js
@@ -11,7 +11,7 @@ $(function(){
         + " sail, and program.<br><br>I've put most of my projects on "
         + "<a href='http://github.com/OBRIEN66'>Github</a>.<br><br>Namely,"
         + " <ul><li><a href='http://github.com/OBRIEN66/terminal-like-site' target='_blank'>This</a></li>"
-        + "<li><a href='http://obrien.tech' target='_blank'>My other site</a></li></ul><br>I'm a part of the "
+        + "<li><a href='https://obrien66.github.io' target='_blank'>My other site</a></li><li><a href='https://world.ac' target='_blank'>world.ac</a> <a href='https://github.com/malsf21/world.ac' target'_blank'>(repo)</a></li></ul><br>I'm a part of the "
         + "<a href='http://robotics.ucc.on.ca' target='_blank'>robotics club</a> at my school and"
         + " I did a little bit of work on the site. <br><br> "
         + "This year, I will take part in <a href='http://world.ac' target='_blank'>World Affairs Conference</a>"
